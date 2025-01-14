4669ccfb72057691e3d115e8f15875e7512b297d
Ashit Kumar Rai <68168801+ashitkumarrai@users.noreply.github.com>
2025-01-14 06:25:17 +0530

    Feat- Initial setup

---
+++ src/main/java/com/example/fastProductApi/FastProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastProductApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fastProductApi/FastProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastProductApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
