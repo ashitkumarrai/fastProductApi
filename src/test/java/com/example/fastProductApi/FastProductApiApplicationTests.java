1e2e123b954c471af050a5751de60ae8a7b4a7ba
Ashit Kumar Rai <68168801+ashitkumarrai@users.noreply.github.com>
2025-01-16 12:37:01 +0530

    Update FastProductApiApplicationTests.java

--- src/test/java/com/example/fastProductApi/FastProductApiApplicationTests.java
+++ src/test/java/com/example/fastProductApi/FastProductApiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.fastProductApi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class FastProductApiApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Assert that the application context is not null
+        assertNotNull(applicationContext, "The application context should have loaded.");
+    }
 
 }
